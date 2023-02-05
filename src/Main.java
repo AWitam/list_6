e1f1fc1ff1ad64d96ffd7f6c6880ceee12462ad4
Ada Witamborska <adrianna@tonik.com>
2023-02-05 18:04:08 +0100

    refactor: add observer demoto Main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import builder_pattern.BuilderPatternDemo;
 import flyweight_pattern.FlyweightPatternDemo;
+import observer_pattern.ObserverPatternDemo;
 
 public class Main {
     public static void main(String[] args) {
         BuilderPatternDemo.main(args);
         FlyweightPatternDemo.main(args);
+        ObserverPatternDemo.main(args);
     }
 }
